f49452a866a9a414fc50cbc658deadce2ea62685
Samson B Haile <shaile@nxnode-0.rd.unr.edu>
2017-02-20 01:36:45 -0800

    buffer size mod

--- PA1/proj1.c
+++ PA1/proj1.c
@@ -16,8 +16,8 @@ MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
 MPI_Get_processor_name(hostname, &len);
 
 int size;
-int max = 100000;
-int interval = 1;
+int max = 10000000;
+int interval = 1000;
 
 int* val = malloc(sizeof(int) * max);
 
