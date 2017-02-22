abcc995aa1d3211e851c2f393f3ea7317d73ba15
Samson B Haile <shaile@nxnode-0.rd.unr.edu>
2017-02-21 19:40:52 -0800

    checking to see how cl arguments work in mpi

--- PA1/proj1.c
+++ PA1/proj1.c
@@ -9,14 +9,15 @@ int main(int argc, char *argv[])
 {
 int numtasks, taskid,len;
 char hostname[MPI_MAX_PROCESSOR_NAME];
-
+printf("%d", argc);
 MPI_Init(&argc, &argv);
+printf("%d", argc);
 MPI_Comm_size(MPI_COMM_WORLD,&numtasks);
 MPI_Comm_rank(MPI_COMM_WORLD,&taskid);
 MPI_Get_processor_name(hostname, &len);
 
 int size;
-int max = 500000;
+int max = 10000;
 int interval = 1;
 
 int* val = malloc(sizeof(int) * max);
