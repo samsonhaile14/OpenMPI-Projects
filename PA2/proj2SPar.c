0f2d96a510728745141d7a856031616f8e7a2f87
samsonhaile28 <shaile@nevada.unr.edu>
2017-03-12 04:17:51 -0700

    finalized version of programs for data collection

--- PA2/proj2SPar.c
+++ PA2/proj2SPar.c
@@ -90,7 +90,7 @@ int main(int argc, char *argv[])
 		}
 
 		//distribute and receive work
-		for(disp_width = 500; disp_width <= 500; disp_width ++){
+		for(disp_width = 500; disp_width <= max_width; disp_width ++){
 			disp_height = disp_width;
 
 			//start timer
@@ -157,10 +157,10 @@ int main(int argc, char *argv[])
 
 			//write image to file (uncomment following two lines if image is desired)
 			//smallest sized image (500x500) will be displayed
-			if(disp_width == 500){
-			pim_write_black_and_white("mandelbrotImg", disp_height, 
-						  disp_width,image);
-			}
+			//if(disp_width == 500){
+			//pim_write_black_and_white("mandelbrotImg", disp_height, 
+			//			  disp_width,image);
+			//}
 
 			//deallocate for iteration
 			free(buffer);
@@ -186,7 +186,7 @@ int main(int argc, char *argv[])
 
 		//receive coordinate range and compute image
 		//note: code was taken from textbook and modified
-		for(disp_width = 500; disp_width <= 500; disp_width ++){
+		for(disp_width = 500; disp_width <= max_width; disp_width ++){
 			disp_height = disp_width;
 
 			scale_real = (real_max - real_min)/((double)disp_width);
