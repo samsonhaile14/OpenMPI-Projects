4c68bb11a70429f4a22ed26f5eb8d98a2c9f7162
samsonhaile28 <shaile@nevada.unr.edu>
2017-03-15 20:48:58 -0700

    size image

--- PA2/proj2Seq.c
+++ PA2/proj2Seq.c
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
 	//initialization
 	MPI_Init(&argc, &argv);	//only used for timer
 
-	max_width = max_height = 30000;
+	max_width = max_height = 10000;
 	real_min = -2;
 	real_max = 2;
 	imag_min = -2;
@@ -100,7 +100,7 @@ int main(int argc, char *argv[])
 	}
 
 	//free memory and terminate
-	for( x = 0; x < disp_width; x++ ){
+	for( x = 0; x < max_width; x++ ){
 		free(image[x]);
 	}
 
