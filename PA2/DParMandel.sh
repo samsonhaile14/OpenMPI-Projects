19571b4ae6be8430fe129cec8e01700ad0037b3b
samsonhaile28 <shaile@nevada.unr.edu>
2017-03-18 04:04:14 -0700

    DParMandel.sh SParMandel.sh

--- PA2/DParMandel.sh
+++ PA2/DParMandel.sh
@@ -2,8 +2,6 @@
 #SBATCH -n 32
 #SBATCH -N 4
 #SBATCH --mem=2048MB
-#SBATCH --time=00:30:00
-#SBATCH --mail-user=shaile@h1.cse.unr.edu
-#SBATCH --mail-type=ALL
+#SBATCH --time=00:05:00
 
 srun PA2DPar
--- PA2/SParMandel.sh
+++ PA2/SParMandel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-#SBATCH -n 2
+#SBATCH -n 32
+#SBATCH -N 4
 #SBATCH --mem=2048MB
-#SBATCH --time=00:30:00
+#SBATCH --time=00:05:00
 #SBATCH --mail-user=shaile@h1.cse.unr.edu
 #SBATCH --mail-type=ALL
 
