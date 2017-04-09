b4bcffc1a9ddce585f76330f6ad7a27c36a951a8
Samson B Haile <shaile@h1.cse.unr.edu>
2017-04-08 21:38:12 -0700

    Adding sequential data

--- PA3/seqSort.sh
+++ PA3/seqSort.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -n 1
-#SBATCH --mem=4096MB
+#SBATCH --mem=2048MB
 #SBATCH --time=00:30:00
 #SBATCH --mail-user=shaile@h1.cse.unr.edu
 #SBATCH --mail-type=ALL
 
-srun PA3Seq 2
+srun PA3Seq 32 4700000
