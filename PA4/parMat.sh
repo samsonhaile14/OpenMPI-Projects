2fce09e37f857cb7d07e3983914a0ef6d5bc9fc2
Samson B Haile <shaile@h1.cse.unr.edu>
2017-04-19 20:02:49 -0700

    Parameter changes to batch file

--- PA4/parMat.sh
+++ PA4/parMat.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-#SBATCH -n 3
+#SBATCH -n 16
+#SBATCH -N 2
 #SBATCH --mem=2048MB
 #SBATCH --time=00:30:00
 #SBATCH --mail-user=shaile@h1.cse.unr.edu
 #SBATCH --mail-type=ALL
 
-srun PA4Par 5
+srun PA4Par 5800 5
