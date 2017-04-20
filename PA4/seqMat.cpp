6780fb701f2a734e19c430b07290019d0b93898c
Samson Haile <samsonhaile14@gmail.com>
2017-04-19 18:38:39 -0700

    Modified arrangement of memory in sequential

--- PA4/seqMat.cpp
+++ PA4/seqMat.cpp
@@ -8,14 +8,15 @@
 
 using namespace std;
 
+void transpose(vector< int > &matB, long long int max_width)
+
 //main program
 int main(int argc, char *argv[])
 {
 
 	//variables
 	long long int index, jndex, kndex;
-	int x,y, max_width, max_height,disp_width,disp_height;
-	
+	int x,y, max_width, max_height,disp_width,disp_height;	
 
 	if(argc < 1){
 		return 1;
@@ -27,19 +28,20 @@ int main(int argc, char *argv[])
 	MPI_Init(&argc, &argv);	//only used for timer
 	
 	//Set appropriate number of elements per matrix
-		vector<long long int> vTemp(max_width, 0);
-		vector< vector<long long int> > matA(max_height,vTemp);
-		vector< vector<long long int> > matB(max_height,vTemp);
-		vector< vector<long long int> > result(max_height,vTemp);
-		vTemp.clear();
+		vector< int > matA(max_height*max_width,0);
+		vector< int > matB(max_height*max_width,0);
+		vector< long long int > result(max_height*max_width,0);
 		
 	//designate matrix values
-	for(index = 0; index < max_width; index++){
-		for(jndex = 0; jndex < max_height; jndex++){
-			matA[index][jndex] = (1 + (random() % 9999));
-			matB[index][jndex] = (1 + (random() % 9999));
+	for(index = 0; index < max_height; index++){
+		for(jndex = 0; jndex < max_width; jndex++){
+			matA[index*max_width + jndex] = (1 + (random() % 9999));
+			matB[index*max_width + jndex] = (1 + (random() % 9999));
 		}
 	}
+	
+	//transpose matB for contiguous access
+	transpose(matB, max_width);
 			
 	//compute product
 	for(disp_width = max_width / 5; disp_width <= max_width; disp_width += max_width / 5){
@@ -49,10 +51,10 @@ int main(int argc, char *argv[])
 		double start = MPI_Wtime();
 
 		//matrix multiply
-		for(index = 0; index < disp_width; index++){
-			for(jndex = 0; jndex < disp_height; jndex++){
+		for(index = 0; index < disp_height; index++){
+			for(jndex = 0; jndex < disp_width; jndex++){
 				for(kndex = 0; kndex < disp_width; kndex++){
-					result[index][jndex] += matA[index][kndex] * matB[kndex][jndex];
+					result[index*max_width + jndex] += (long long int) matA[index*max_width + kndex] * (long long int) matB[jndex*max_width + kndex];
 				}
 			}
 		}
@@ -67,5 +69,21 @@ int main(int argc, char *argv[])
 
 	MPI_Finalize();
 
+}
+
+//function
+void transpose(vector< int > &matB, long long int max_width){
+	
+	long long int index, jndex;
+	int temp;
+	
+	for(index = 0; index < max_width;index++){
+		for(jndex = index + 1; jndex < max_width; jndex++){
+			temp = matB[index * max_width + jndex];
+			matB[index * max_width + jndex] = matB[jndex * max_width + index];
+			matB[jndex * max_width + index] = temp;
+		}
+	}
+	
 }
 
