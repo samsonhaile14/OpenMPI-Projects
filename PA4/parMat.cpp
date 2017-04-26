b33fc05204558cbd58f9cffb9629f14a969286c3
Samson Haile <samsonhaile14@gmail.com>
2017-04-26 13:05:33 -0700

    testing

--- PA4/parMat.cpp
+++ PA4/parMat.cpp
@@ -53,6 +53,8 @@ int main(int argc, char *argv[])
 		outputResults = (bool)atoi(argv[6]);
 	}
 	
+	printf(outputResults ? "true" : "false");
+	
 	max_width = max_height = atoll(argv[1]);
 	sub_sizes = atoll(argv[2]);
 	
