bb2c61df6b7ef6d2d86376d572c6275d758e0044
Samson B Haile <shaile@h1.cse.unr.edu>
2017-04-26 14:07:28 -0700

    updating finalized batch files

--- PA4/parMat.sh
+++ PA4/parMat.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -n 16
-#SBATCH -N 2
+#SBATCH -n 2
+#SBATCH -N 1
 #SBATCH --mem=2048MB
 #SBATCH --time=00:30:00
 #SBATCH --mail-user=shaile@h1.cse.unr.edu
 #SBATCH --mail-type=ALL
 
-srun PA4Par 5800 5
+srun PA4Par 5800 1 inp1.txt inp1.txt 1
