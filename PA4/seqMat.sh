8338afd93b102e1ed1f345adfa218d4ccfb74fbf
Samson B Haile <shaile@h1.cse.unr.edu>
2017-05-02 22:58:53 -0700

    Data and last used configurations for program

--- PA4/seqMat.sh
+++ PA4/seqMat.sh
@@ -2,7 +2,8 @@
 #SBATCH -n 1
 #SBATCH --mem=2048MB
 #SBATCH --time=00:30:00
+#SBATCH --output=seqTest.txt
 #SBATCH --mail-user=shaile@h1.cse.unr.edu
 #SBATCH --mail-type=ALL
 
-srun PA4Seq 5800 5 inp1.txt inp1.txt 1
+srun PA4Seq 4320 10 inp0.txt inp0.txt 0
