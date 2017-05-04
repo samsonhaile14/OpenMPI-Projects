7857885f200384be29b7ad5333b7432458e0331d
Samson B Haile <shaile@h1.cse.unr.edu>
2017-05-04 01:05:22 -0700

    adding sample config of batch

--- PA4/parMat.sh
+++ PA4/parMat.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-#SBATCH -n 2
-#SBATCH -N 1
+#SBATCH -n 16
+#SBATCH -N 2
 #SBATCH --mem=2048MB
 #SBATCH --time=00:30:00
+#SBATCH --output=parc16-1.txt
 #SBATCH --mail-user=shaile@h1.cse.unr.edu
 #SBATCH --mail-type=ALL
 
-srun PA4Par 5800 1 inp1.txt inp1.txt 1
+srun PA4Par 5280 10 inp0.txt inp0.txt 0 1
